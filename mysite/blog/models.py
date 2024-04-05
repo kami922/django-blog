127e87c043c08fcd08aead472536a1ade14ba18c
kami922 <kamranulhaq2002@gmail.com>
2024-04-05 21:13:20 +0500

    done till section 2 and edit ignore file

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -24,6 +25,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default="draft")
+    tags = TaggableManager()
     
     def __str__(self):
             return self.title
@@ -37,6 +39,21 @@ class Post(models.Model):
     
     objects = models.Manager()
     published = PublishedManager()        
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ("created",)
+    
+    def __str__(self):
+        return f"Commented by {self.name} on {self.post}"
         
         
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,32 +1,37 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import  EmailPostForm
-
+from .forms import  EmailPostForm,CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list,3)#3 posts in each page
-#     page = request.GET.get("page")
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # if page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # if page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,'blog/post/list.html',{"page":page,"posts":posts})
+def post_list(request,tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list,3)#3 posts in each page
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{"page":page,"posts":posts,'tag':tag})
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html' 
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html' 
 
 
 def post_detail(request,year,month,day,post):
@@ -35,7 +40,25 @@ def post_detail(request,year,month,day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request,"blog/post/detail.html",{'post':post})
+    #list pf active comments for this post
+    comments = post.comments.filter(active = True)
+    new_comment = None
+    if request.method == "POST":
+        #a comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #create comment object but  dont save to database yet
+            new_comment = comment_form.save(commit=False)
+            #assign current post to comment
+            new_comment.post = post
+            #save comment to database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list("id",flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by("-same_tags","-publish")[:4]
+    return render(request,"blog/post/detail.html",{'post':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form,'somilar_posts':similar_posts})
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status="published")
@@ -45,9 +68,9 @@ def post_share(request,post_id):
         if form.is_valid():
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url)
-            subject = f"{cd["name"]}recommends you read {post.title}"
+            subject = f"{cd['name']} recommends you read {post.title}"
             message = F"Read {post.title} at {post_url} \n\n {cd['name']}'s comments: {cd['comments']}"
-            send_mail(subject,message,"admin@django.com",[cd["to"]])
+            send_mail(subject,message,"kamranulhaq2002@gmail.com",[cd["to"]])
             sent = True
             
     else:
